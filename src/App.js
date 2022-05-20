b7811dd39df26ed607a4448b1e53925135d6b61c
Dixen-Naxos <armanddailly@gmail.com>
2022-05-20 19:18:43 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState('')
+    const url = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=db988691faf182dfc3750cd1e57f3718`
+    //const url2 = 'https://api.openweathermap.org/data/2.5/onecall?lat=51.5073219&lon=-0.1276474&exclude=minutely,hourly,alerts&units=metric&appid=db988691faf182dfc3750cd1e57f3718'
+
+    const searchLocation = (event) => {
+        if(event.key === 'Enter') {
+            axios.get(url).then((response)=>{
+                setData(response.data)
+                console.log(data[0])
+            })
+        }
+    }
+
+    return (
+        <div className="App">
+            <div className="search">
+                <input value={location}
+                       onChange={event => setLocation(event.target.value)}
+                       onKeyPress={searchLocation}
+                       placeholder='Enter Location'
+                       type="text"/>
+            </div>
+            <h1>The Forecast Weather App</h1>
+            <main>
+
+            </main>
+            <footer>
+                Page created by Armand DAILLY
+            </footer>
+        </div>
+    );
 }
 
 export default App;
+
+
+//const url = 'https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=db988691faf182dfc3750cd1e57f3718'
+//result[0]["lat"]
+//result[0]["lon"]
+
+//const url2 = 'https://api.openweathermap.org/data/2.5/onecall?lat=51.5073219&lon=-0.1276474&exclude=minutely,hourly,alerts&units=metric&appid=db988691faf182dfc3750cd1e57f3718'
---
+++ src/components/forecast/Forecast.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+const Forecast = () => {
+    //const url = 'https://api.openweathermap.org/geo/1.0/direct?q=${}&limit=5&appid=db988691faf182dfc3750cd1e57f3718'
+    //const url2 = 'https://api.openweathermap.org/data/2.5/onecall?lat=51.5073219&lon=-0.1276474&exclude=minutely,hourly,alerts&units=metric&appid=db988691faf182dfc3750cd1e57f3718'
+    function getForecast() {
+
+    }
+
+    return (<div></div>)
+}
+
+export default Forecast;
