70216107d47f814e3cded1e57e16b8c4a9093abe
Dixen-Naxos <armanddailly@gmail.com>
2022-05-22 15:52:06 +0200

    WIP Front

--- front/src/components/forecast/Day.js
+++ front/src/components/forecast/Day.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 function Day(props) {
+    const icon = "https://openweathermap.org/img/wn/" + props.data.icon + "@4x.png"
+    //<img src={icon} alt="icone"/>
     return (
         <div>
-            <div>image</div>
+            <div>{icon}</div>
             <div>{props.data.dt}</div>
-            <div>{props.data.temp_day}</div>
+            <div>{props.data.temp_day} °C</div>
         </div>)
 }
 
--- front/src/components/forecast/Today.js
+++ front/src/components/forecast/Today.js
@@ -2,21 +2,23 @@ import React from "react";
 import axios from "axios";
 
 function Today(props){
+    const icon = "https://openweathermap.org/img/wn/" + props.data.icon + "@4x.png"
+    //<img src={icon} alt="icone"/>
     return (
         <div>
             <div>
-                <div>image</div>
-                <div>{props.data.temp_day}</div>
+                <div>{icon}</div>
+                <div>{props.data.dt}</div>
             </div>
             <div>
-                <div>{props.data.temp_day}</div>
-                <div>{props.data.pressure}</div>
+                <div>{props.data.temp_day}°C</div>
+                <div>{props.data.pressure}hPa</div>
             </div>
             <div>
-                <div>{props.data.temp_night}</div>
-                <div>{props.data.wind_speed}</div>
+                <div>{props.data.temp_night}°C</div>
+                <div>{props.data.wind_speed}Km/h</div>
             </div>
-            <div>{props.data.humidity}</div>
+            <div>{props.data.humidity}%</div>
         </div>)
 
 }
